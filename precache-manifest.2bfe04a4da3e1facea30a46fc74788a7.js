f2775a35920dc8eef363aac642e7df2bca726ace
andrayantelo <andrayantelo@gmail.com>
2020-03-13 17:14:48 -0700

    Updates

---
+++ precache-manifest.2bfe04a4da3e1facea30a46fc74788a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50472171462350cdb506bf921e6cfb3",
+    "url": "/react-quote-generator/index.html"
+  },
+  {
+    "revision": "1093f2be6055c88358f5",
+    "url": "/react-quote-generator/static/css/main.f2540bdf.chunk.css"
+  },
+  {
+    "revision": "05617490d594189a502e",
+    "url": "/react-quote-generator/static/js/2.75948fc0.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/react-quote-generator/static/js/2.75948fc0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1093f2be6055c88358f5",
+    "url": "/react-quote-generator/static/js/main.532fd34d.chunk.js"
+  },
+  {
+    "revision": "027e7920eb830aac32f7",
+    "url": "/react-quote-generator/static/js/runtime-main.b2f60342.js"
+  }
+]);
